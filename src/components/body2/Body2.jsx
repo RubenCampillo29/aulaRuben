e6771d2b9c65cec261aa93e1cc4398dc12ebb3a5
ruben <3886114@example.com>
2023-05-16 11:47:01 +0200

    holamndo

--- src/components/body2/Body2.jsx
+++ src/components/body2/Body2.jsx
@@ -16,19 +16,37 @@ export default function Body2() {
                                    <img class="user-foto" src="https://aulavirtual.murciaeduca.es/theme/image.php/moove/core/1677155381/u/f1" alt="No carga" />
                               </div>
                               <div class="texto">
-                                   <p class="letras">¡¡Actualizamos plataforma!! <br>
-                                   </br>
+                                   <p class="letras">¡¡Actualizamos plataforma!!  </p>
+                                   
                                         de <a href="">admin Aulavirtual</a> - Martes, 27 de Juliode 2021, 09:02
-                                   </p>
-                                   <br />
-                                   <div>
+                                  
+                                   <br/>
+                                   <div><br/>
                                         Este mes de Julio se ha actualizado la plataforma Moodle a la version 3.9 (LTS)
                                    </div>
                                    <br />
                                    <div>
-                                        "A nivel de usuario  <br />
-                                        gjfjffjfikh   "
-
+                                        "A nivel de usuario uno de los cambios más relevantes es el del cambio de tema porque el anterior,
+                                         Essential, ya no está soportado en las últimas versiones de Moodle. Si desea cambiar la apariencia de su curso puede 
+                                         hacerlo en la configuración de éste eligiendo uno de los temas disponibles en esta versión.  "
+                                   </div>
+                                   <br />
+                                   <div>
+                                     Otras mejoras en la actualizacion
+                                     <ul className="ulu">
+                                       <li>Cambios en la interfaz de Moodle.</li>
+                                       <li>Mejor integración con el framework H5P.</li>
+                                       <li>Mejoras y nuevas características en el calificador.</li>
+                                       <li>Mejor integración con la característica "Safe Exam Browser".</li>
+                                       <li>Mejoras en el uso de insignias y soporte de OpenBadget 2.</li>
+                                       <li>Mejoras en la Accesibilidad de la plataforma.</li>
+                                     </ul>
+                                   </div>
+                                   <div className="texto-inferior">
+                                        <ul>
+                                             <li> <a href="">Enlacen permanente</a></li>
+                                             <li><a href="">Hacer un comentario en este tema</a>(0 Replicas)</li>
+                                        </ul>
                                    </div>
                               </div>
                          </div>
